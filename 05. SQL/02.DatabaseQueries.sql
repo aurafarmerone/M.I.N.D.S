8f236fcd6865ba1f7ff85f3bb39dacd23cb1f7de
aurafarmerone <aurafarmerone@gmail.com>
2026-02-11 21:25:56 +0530

    Adding DatabaseQueries

---
+++ 05. SQL/02.DatabaseQueries.sql
@@ -0,0 +1,15 @@
+# Database Queries
+
+# CREATE DATABASE Google;
+CREATE DATABASE IF NOT EXISTS Google; # just show warning not error
+
+SHOW DATABASES;
+
+USE Google;
+SHOW TABLES; # display nothing becuase no table in this database
+
+USE aiml;
+SHOW TABLES;
+
+
+
